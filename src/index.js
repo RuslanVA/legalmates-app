700ed9d55bd8e05e069c6206d25a8e61038e1d17
Ruslan Vahapov <ruslan.vahapov@otakoyi.com.ua>
2020-02-11 13:22:50 +0200

    add base,fonts,mixins,variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './assets/scss/style.scss'
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
